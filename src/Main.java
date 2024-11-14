1c446b0d6f227cbc6824a5827eb20e171f6a73e7
v-nebe <katya.shauneva@gmail.com>
2024-11-14 13:46:58 +0300

    Calculator

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите первое число: ");
-        int firstNumber = in.nextInt();
+        double firstNumber = in.nextDouble();
         System.out.println("Введите второе число: ");
-        int secondNumber = in.nextInt();
+        double secondNumber = in.nextDouble();
         System.out.println("Выберите один из операторов: +, -, *, /");
         String operator = in.next();
         solution(firstNumber, secondNumber, operator);
     }
 
-    public static int solution(int firstNumber, int secondNumber, String operator){
-        int result = 0;
+    public static void solution(double firstNumber, double secondNumber, String operator){
 
+        double result = 0;
         switch (operator) {
-            case "+" -> result = firstNumber + secondNumber;
-            case "-" -> result = firstNumber - secondNumber;
-            case "*" -> result = firstNumber * secondNumber;
-            case "/" -> result = firstNumber / secondNumber;
-            case null, default -> System.out.println("Выберите верный оператор");
+            case "+":
+                result = firstNumber + secondNumber;
+                System.out.println("Ваш результат: " + result);
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                System.out.println("Ваш результат: " + result);
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                System.out.println("Ваш результат: " + result);
+                break;
+            case "/":
+                result = firstNumber / secondNumber;
+                System.out.println("Ваш результат: " + result);
+                break;
+            default:
+                System.out.println("Выберите верный оператор");
+                break;
         }
-        System.out.println("Ваш результат: " + result);
-        return result;
     }
 }
